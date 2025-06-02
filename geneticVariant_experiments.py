b1295676c164f63dcee426a99bd1d9c0ecb36aee
Clara De Paolis <claradepaolis@users.noreply.github.com>
2025-06-02 01:25:52 -0400

    case study

--- geneticVariant_experiments.py
+++ geneticVariant_experiments.py
@@ -4,13 +4,11 @@ import os
 import pickle
 from tqdm import tqdm
 
-from utils.Mixture import NMixture, PUMixture
-import utils.mixtureUtils as mixtureUtils
-from utils.NestedGroupDistUnknownGroup import NestedGroupDistUnknownGroup
-import utils.correct_metrics as correct_metrics
-import utils.correct_metrics_unknowngroups as correct_metrics_unknowngroups
-
-
+from Mixture import NMixture, PUMixture
+import mixtureUtils
+from pnuem.NestedGroupDistUnknownGroup import NestedGroupDistUnknownGroup
+import correct_metrics_unknowngroups
+import correct_metrics
 
 
 def parse_args():
@@ -18,6 +16,8 @@ def parse_args():
     parser = argparse.ArgumentParser('get_fairness_metrics')
     parser.add_argument("--output", type=str, default="variants_results",
                         help="Path to file where the metrics are to be stored")
+    parser.add_argument("--saveparams", action="store_true",
+                        help="flag to save estimated model parameters (large files)")
     parser.add_argument("--dim", type=int, default=None,
                         help="Dimension of synthetic data")
     parser.add_argument("--comp", type=int, default=None,
@@ -63,10 +63,7 @@ def main():
 
     num_groups = 2
     classifier_threshold = args.thresh
-    calibrated_thresholds = {'benign_supporting': 0.391,
-                             'benign_moderate': 0.197,
-                             'benign_strong': 0.010,
-                             'even': 0.5,
+    calibrated_thresholds = {'even': 0.5,
                              'supporting': 0.737,
                              'moderate': 0.829,
                              'strong': 0.932
@@ -85,7 +82,7 @@ def main():
     dataset = np.load(data_path, allow_pickle=True)
 
     for dim in dimensions:
-        for comp in components:
+        for comp in tqdm(components):
             print(f"Dimension: {dim}, Components: {comp}")
 
             estimated_measures_ng = {measure: {"g1_l": list(), "g2_l": list(), "g1_ul": list(), "g2_ul": list(),
@@ -155,37 +152,24 @@ def main():
             print('Estimating selection bias model parameters')
             Kfit = [comp, comp]
             nested_group_EM = NestedGroupDistUnknownGroup(
-                x_unlabeled=s_ub, x_labeled=[s_neg_bias, s_pos_bias],
+                x_unlabeled=s_ub, x_labeled=[s_pos_bias, s_neg_bias],
                 unlabeled_groups=groups_ub,
                 components=Kfit, num_classes=2, num_groups=2)
             nested_group_EM.estimate_params(max_steps=1000)
             print('Parameters estimated')
 
             alphas_1_est, alphas_2_est = nested_group_EM.alphas
-            w_1_est = [nested_group_EM.w[c_label][0] for c_label in [0, 1]]  # group==0 for each class (label)
-            wl_est = [nested_group_EM.w_labeled[c_label] for c_label in [0, 1]]
-            w_2_est = [nested_group_EM.w[c_label][1] for c_label in [0, 1]]  # group==1 for each class (label)
+            w_1_est = [nested_group_EM.w[c][0] for c in [0, 1]]  # group==0 for each class
+            wl_est = [nested_group_EM.w_labeled[c] for c in [0, 1]]
+            w_2_est = [nested_group_EM.w[c][1] for c in [0, 1]]  # group==1 for each class
             lls_1 = lls_2 = nested_group_EM.lls
 
-            for thresh_name, class_threshold in tqdm(zip(thresh_names, class_thresholds), total=len(class_thresholds)):
-                if 'benign' in thresh_name:
-                    # flip comparison direction
-                    y_all_bias = dataset['labeled_scores'] <= class_threshold
-                    y_all_ub = dataset['unlabeled_scores'][select] <= class_threshold
-                    # flip meaning of "positive"
-                    neg_label = 1
-                    pos_label = 0
-                    alpha_l = 1 - alpha_l
+            for thresh_name, class_threshold in zip(thresh_names, class_thresholds):
+                y_all_bias = dataset['labeled_scores'] >= class_threshold
+                y_pos_bias = y_all_bias[dataset['labels'] == 1]
+                y_neg_bias = y_all_bias[dataset['labels'] == 0]
 
-                else:
-                    y_all_bias = dataset['labeled_scores'] >= class_threshold
-                    y_all_ub = dataset['unlabeled_scores'][select] >= class_threshold
-                    neg_label = 0
-                    pos_label = 1
-
-                y_pos_bias = y_all_bias[dataset['labels'] == pos_label]
-                y_neg_bias = y_all_bias[dataset['labels'] == neg_label]
-
+                y_all_ub = dataset['unlabeled_scores'][select] >= class_threshold
                 y_g_ub = {g: y_all_ub[groups_ub == g] for g in np.unique(groups_ub)}
 
                 estimated_measures_ng["bias"]['ll'].append(lls_1[-1])
@@ -194,58 +178,57 @@ def main():
                 # Redefining mixtures with estimated parameters.
                 # Distributions with estimated parameters are used for all subsequent operations
                 estimated_d_unbiased = {
-                    # first group
-                    0: {"pos": NMixture(nested_group_EM.mu[pos_label], nested_group_EM.sg[pos_label], np.array(w_1_est[pos_label])),
-                        "neg": NMixture(nested_group_EM.mu[neg_label], nested_group_EM.sg[neg_label], np.array(w_1_est[neg_label]))},
-                    # second group:
-                    1: {"pos": NMixture(nested_group_EM.mu[pos_label], nested_group_EM.sg[pos_label], np.array(w_2_est[pos_label])),
-                        "neg": NMixture(nested_group_EM.mu[neg_label], nested_group_EM.sg[neg_label], np.array(w_2_est[neg_label]))}}
+                    0: {"pos": NMixture(nested_group_EM.mu[0], nested_group_EM.sg[0], np.array(w_1_est[0])),
+                        "neg": NMixture(nested_group_EM.mu[1], nested_group_EM.sg[1], np.array(w_1_est[1]))},
+                    1: {"pos": NMixture(nested_group_EM.mu[0], nested_group_EM.sg[0], np.array(w_2_est[0])),
+                        "neg": NMixture(nested_group_EM.mu[1], nested_group_EM.sg[1], np.array(w_2_est[1]))}}
 
                 estimated_d_biased = {
-                    "pos": NMixture(nested_group_EM.mu[pos_label], nested_group_EM.sg[pos_label], np.array(wl_est[pos_label])),
-                    "neg": NMixture(nested_group_EM.mu[neg_label], nested_group_EM.sg[neg_label], np.array(wl_est[neg_label]))}
+                    "pos": NMixture(nested_group_EM.mu[0], nested_group_EM.sg[0], np.array(wl_est[0])),
+                    "neg": NMixture(nested_group_EM.mu[1], nested_group_EM.sg[1], np.array(wl_est[1]))}
 
-                # Redefining alpha with the estimated parameter for each group
-                alpha_est = [alphas_1_est[pos_label], alphas_2_est[pos_label]]  # todo: change this for > 2 groups
+                # Redefining alpha with the estimated parameter
+                alpha_est = [alphas_1_est[0], alphas_2_est[0]]
 
                 # estimated biases
                 # for a more stable AUC computation, we average 5 estimations of AUC
-                # This is slow and we're not using AUC, so we skip it
-                # num_auc_runs = 5
-                # for g in range(num_groups):
-                #     estimated_measures_ng["bias"][f"aucpp_g{g + 1}"].append(
-                #         np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["pos"], estimated_d_biased["pos"])
-                #                  for _ in range(num_auc_runs)]))
-                #     estimated_measures_ng["bias"][f"aucnn_g{g + 1}"].append(
-                #         np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["neg"], estimated_d_biased["neg"])
-                #                  for _ in range(num_auc_runs)]))
-                #     estimated_measures_ng["bias"][f"aucpn_g{g + 1}"].append(
-                #         np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["pos"], estimated_d_unbiased[g]["neg"])
-                #                  for _ in range(num_auc_runs)]))
+                num_auc_runs = 5
+                for g in range(num_groups):
+                    estimated_measures_ng["bias"][f"aucpp_g{g + 1}"].append(
+                        np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["pos"], estimated_d_biased["pos"])
+                                 for _ in range(num_auc_runs)]))
+                    estimated_measures_ng["bias"][f"aucnn_g{g + 1}"].append(
+                        np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["neg"], estimated_d_biased["neg"])
+                                 for _ in range(num_auc_runs)]))
+                    estimated_measures_ng["bias"][f"aucpn_g{g + 1}"].append(
+                        np.mean([mixtureUtils.auc(estimated_d_unbiased[g]["pos"], estimated_d_unbiased[g]["neg"])
+                                 for _ in range(num_auc_runs)]))
 
                 # Unbiased group distribution
-                estimated_p = {g: PUMixture(estimated_d_unbiased[g]['pos'], estimated_d_unbiased[g]['neg'], alpha_est[g])
-                               for g in range(num_groups)}
+                estimated_p = {
+                    0: PUMixture(estimated_d_unbiased[0]['pos'], estimated_d_unbiased[0]['neg'], alpha_est[0]),
+                    1: PUMixture(estimated_d_unbiased[1]['pos'], estimated_d_unbiased[1]['neg'], alpha_est[1])}
 
                 # Biased group distribution
                 estimated_q = PUMixture(estimated_d_biased['pos'], estimated_d_biased['neg'], alpha_l)
 
                 # Create a distribution object for the entire unlabeled set (not conditioned on a group)
-                est_alpha_u = {pos_label: sum(eta[g] * alpha_est[g] for g in range(num_groups)),
-                               neg_label: sum(eta[g] * (1 - alpha_est[g]) for g in range(num_groups))}
+                est_alpha_u = {1: sum(eta[g] * alpha_est[g] for g in range(num_groups)),
+                               0: sum(eta[g] * (1 - alpha_est[g]) for g in range(num_groups))}
                 est_wPos_u = sum(eta[g] * alpha_est[g] * estimated_d_unbiased[g]['pos'].ps for g in
-                                 range(num_groups)) / est_alpha_u[pos_label]
+                                 range(num_groups)) / est_alpha_u[1]
                 est_wNeg_u = sum(eta[g] * (1 - alpha_est[g]) * estimated_d_unbiased[g]['neg'].ps for g in
-                                 range(num_groups)) / est_alpha_u[neg_label]
-                est_d_pos_ub = NMixture(nested_group_EM.mu[pos_label], nested_group_EM.sg[pos_label], est_wPos_u)
-                est_d_neg_ub = NMixture(nested_group_EM.mu[neg_label], nested_group_EM.sg[neg_label], est_wNeg_u)
-                est_d_ub = PUMixture(est_d_pos_ub, est_d_neg_ub, est_alpha_u[pos_label])
+                                 range(num_groups)) / est_alpha_u[0]
+                est_d_pos_ub = NMixture(nested_group_EM.mu[0], nested_group_EM.sg[0], est_wPos_u)
+                est_d_neg_ub = NMixture(nested_group_EM.mu[1], nested_group_EM.sg[1], est_wNeg_u)
+                est_d_ub = PUMixture(est_d_pos_ub, est_d_neg_ub, est_alpha_u[1])
 
                 est_wPos_l = estimated_d_biased['pos'].ps
                 est_wNeg_l = estimated_d_biased['neg'].ps
 
                 est_d_pos_bias = estimated_d_biased['pos']
                 est_d_neg_bias = estimated_d_biased['neg']
+
 
                 YSoft_u = est_d_ub.pn_posterior(s_ub).reshape(-1, 1)
                 # Since we do not have labels from the unlabeled data,
@@ -268,9 +251,9 @@ def main():
                        for g in range(num_groups)}
 
                 dp_group_difference, dp = correct_metrics_unknowngroups.correct_dp(y_all_bias=y_all_bias,
-                                                                                   y_all_ub=y_g_ub,
-                                                                                   s_all_bias=s_labeled,
-                                                                                   p=estimated_p, q=estimated_q
+                                                                                  y_all_ub=y_g_ub,
+                                                                                  s_all_bias=s_labeled,
+                                                                                  p=estimated_p, q=estimated_q
                                                                                   )
                 dp_bias_withUL = {g: TPR[g] + FPR[g] for g in range(num_groups)}
                 estimated_measures_ng["dp"]['g1_l'].append(dp['dp_bias_withLabelled'][0])
@@ -284,7 +267,7 @@ def main():
                 eo_group_difference, eo = correct_metrics_unknowngroups.correct_eo(
                     y_pos_bias=y_pos_bias,
                     y_pos_ub={0: None, 1: None},  # this is ignored since we don't know the labels
-                    s_pos_bias=s_pos_bias if pos_label==1 else s_neg_bias,
+                    s_pos_bias=s_pos_bias,
                     q_pos=estimated_d_biased["pos"],
                     p_pos={0: estimated_d_unbiased[0]["pos"], 1: estimated_d_unbiased[1]["pos"]})
 
@@ -300,7 +283,7 @@ def main():
                 pe_group_difference, pe = correct_metrics_unknowngroups.correct_pe(
                     y_neg_bias=y_neg_bias,
                     y_neg_ub={0: None, 1: None},  # this is ignored since we don't know the labels
-                    s_neg_bias=s_neg_bias if pos_label == 1 else s_pos_bias,
+                    s_neg_bias=s_neg_bias,
                     q_neg=estimated_d_biased["neg"],
                     p_neg={0: estimated_d_unbiased[0]["neg"], 1: estimated_d_unbiased[1]["neg"]})
 
@@ -333,8 +316,9 @@ def main():
             # Save fairness metrics data as npz files for each dimension
             np.savez(file_path, **{ 'estimated_ng': estimated_measures_ng})
 
-            # Save estimated GMM model to file
-            if not bootstrap:
+            # Save estimated GMM model
+            # save to file
+            if args.saveparams:
                 with open(file_path.split('.npz')[0]+'_gmm_model.pkl', 'wb') as f:
                     pickle.dump(nested_group_EM, f)
 
